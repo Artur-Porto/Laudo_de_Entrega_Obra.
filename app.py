b4bc258ac819eaa583783a13bc1b627a3f3dbd01
Artur Porto <110861215+Artur-Porto@users.noreply.github.com>
2025-06-26 19:29:04 -0300

    Update app.py

--- app.py
+++ app.py
@@ -19,9 +19,8 @@ def analisar_paragrafos(paragraphs, idx_table):
 
     for paragraph in paragraphs:
         texto = paragraph.text.lower()
-        count_nao_conf += len(re.findall(r"não\s*conforme", texto))
-        count_conf += len(re.findall(r"\bconforme\b", texto))
-
+        count_nao_conf += len(re.findall(r"(❌\s*)?Não\s*conforme", texto))
+        count_conf += len(re.findall(r"(✔️\s*)?Conforme", texto))
         if "descrição" in texto:
             passou_por_descricao = False
             texto_runs = []
